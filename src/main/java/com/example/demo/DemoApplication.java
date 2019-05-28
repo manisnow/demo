ea8e68ae09646cc04b44699bea15275edb3f3dc3
manisnow <manisnow@gmail.com>
2019-05-28 15:48:06 -0400

    data  grid

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,47 @@
 package com.example.demo;
 
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.eviction.EvictionStrategy;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.spring.embedded.provider.SpringEmbeddedCacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
 public class DemoApplication {
+
+	@Autowired
+	SpringEmbeddedCacheManager cacheManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
 	@RequestMapping("/")
-	public String hello() {	
-		return "Hello World";		
+	public String hello() {
+		return "Hello World";
+	}
+
+	@RequestMapping("/catch")
+	public String catchT() {
+		cacheManager.getCacheNames().iterator().forEachRemaining(c -> System.out.println(c));
+		return "Hello World";
+	}
+
+	@Bean
+	public SpringEmbeddedCacheManager cacheManager() throws Exception {
+		Configuration config = new ConfigurationBuilder().eviction().strategy(EvictionStrategy.LRU).maxEntries(150)
+				.build();
+
+		return new SpringEmbeddedCacheManager(new DefaultCacheManager(config));
 	}
 
 }
